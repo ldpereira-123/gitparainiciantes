5a6101c1195e11b2f66ffc7bd9c6e16f8ba11789
Leandro de Matos Pereira <ldpereira@uolinc.com>
2019-05-02 13:09:39 -0300

    iniciando nova aplicação API

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.status(200).send({ message: 'olá mundo estou funcionando!' });
+})
+
+app.get('/ola', (req, res) => {
+	res.status(200).send({ message: 'Eu sou o Olá' });
+})
+
+app.listen(4001, () => {
+	console.log('Api rodando na porta 4001');
+})
+	
